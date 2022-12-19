2144b68e5d8b05827d743fd41b45931345b9e61b
Slavbor <shipmate@mail.ru>
2022-12-20 00:01:40 +0300

    zadacha 15 ready

---
+++ Zadacha15/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+Write("Enter the week day number : ");
+int num = Convert.ToInt32(ReadLine());
+if(num >= 1 && num <= 5)
+{
+    WriteLine("No");
+}
+else if(num == 6 || num == 7)
+{
+    WriteLine("Yes");
+}
+else
+{
+    WriteLine("Wrong number");
+}
