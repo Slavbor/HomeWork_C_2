ce06ca94349bd53ca5758cab90648882d8fd07ab
Slavbor <shipmate@mail.ru>
2022-12-19 23:51:01 +0300

    zadacha 13 ready

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+Write("Input number: ");
+int num = Convert.ToInt32(ReadLine());
+if(num > 999)
+{
+    while(num / 10 > 100)
+    {
+        num = num / 10;
+        
+    }
+    WriteLine(num % 10);
+}
+else if(num < 1000 && num > 99)
+{
+    WriteLine(num % 10);
+}                         
+else if(num < 100)
+{
+    WriteLine("NO 3rd digit");
+}       
